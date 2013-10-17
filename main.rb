00bc5b4cebf6dfa913e67536ab6b9f9447e72d90
David Fedele <dave.fedele@gmail.com>
2013-10-16 23:39:44 -0700

    added game route setup main variables for deck/player/dealer/dealing cards and test print

--- main.rb
+++ main.rb
@@ -21,5 +21,23 @@ post '/username' do
 end
 
 get '/game' do
+  # deck 
+  suits = ['H', 'D', 'C', 'S']
+  values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle!
+
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+
+  2.times do
+    session[:dealer_cards] << session[:deck].pop 
+    session[:player_cards] << session[:deck].pop 
+  end
+
+
+  # deal cards
+  #   player cards
+  #   dealer cards
+
   erb :game
 end
