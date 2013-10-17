766d4c3540f813da1c40b6d30d10c4eb75326ae3
David Fedele <dave.fedele@gmail.com>
2013-10-17 00:05:49 -0700

    added helper to calculate_total for player and dealer hands

--- main.rb
+++ main.rb
@@ -3,6 +3,33 @@ require 'sinatra'
 
 set :sessions, true
 
+helpers do
+
+  def calculate_total(cards) # cards is [['D', '4'], ['H', 'A'] ...]
+    arr = cards.map {|e| e[1]}
+
+    total = 0
+
+    arr.each do |value|
+      if value == 'A'
+        total += 11
+      else 
+        total += value.to_i == 0 ? 10 : value.to_i
+      end
+    end
+
+    #correct for Aces
+    arr.select {|v| v == 'A'}.count do
+      break if total <= 21
+      total -=10
+    end
+
+    total
+  end
+
+end
+
+
 get '/' do
   if session[:username]
     redirect '/game'
@@ -24,7 +51,7 @@ get '/game' do
   # deck 
   suits = ['H', 'D', 'C', 'S']
   values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-  session[:deck] = suits.product(values).shuffle!
+  session[:deck] = suits.product(values).shuffle! # cards is [['D', '4'], ['H', 'A'] ...]
 
   session[:dealer_cards] = []
   session[:player_cards] = []
@@ -34,10 +61,13 @@ get '/game' do
     session[:player_cards] << session[:deck].pop 
   end
 
-
-  # deal cards
-  #   player cards
-  #   dealer cards
-
   erb :game
+end
+
+post '/player_turn' do
+  if params[:hit]
+    "You Hit"
+  else
+    "You Stayed"
+  end
 end
