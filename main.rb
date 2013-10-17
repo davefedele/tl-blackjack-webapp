0b66c96550af7fde49f2882d0ea679577291a2ba
David Fedele <dave.fedele@gmail.com>
2013-10-16 23:08:49 -0700

    added username to session. also welcome page and start over work

--- main.rb
+++ main.rb
@@ -4,15 +4,26 @@ require 'sinatra'
 set :sessions, true
 
 get '/' do
+  if session[:username]
+    redirect '/welcome'
+  else
+    erb :username
+  end
+end
+
+get '/username' do
   erb :username
 end
 
 post '/username' do
   session[:username] = params[:username]
-  "hello #{session[:name]}"
   redirect '/welcome'
 end
 
 get '/welcome' do
   erb :welcome
+end
+
+get '/game' do
+  "game"
 end
